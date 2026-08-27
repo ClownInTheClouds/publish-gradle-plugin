25b12b1b71e2d6f0ce675a95147e0b248e457d02
Sorokin Anton <sorokin.a.developer@gmail.com>
2026-08-27 13:37:08 +0500

    remove default conventions for group, version, artifactId (breaking change)

--- src/main/java/dev/sorokin/gradle/publishplugin/GradlePublishPluginExtension.java
+++ src/main/java/dev/sorokin/gradle/publishplugin/GradlePublishPluginExtension.java
@@ -8,15 +8,15 @@ import javax.inject.Inject;
 public abstract class GradlePublishPluginExtension {
 
     private final Property<String> publicationGroup;
-    private final Property<String> publicationArtifactId;
     private final Property<String> publicationVersion;
+    private final Property<String> publicationArtifactId;
     private final Property<PublicationType> publicationType;
 
     @Inject
     public GradlePublishPluginExtension(ObjectFactory objects) {
-        this.publicationGroup = objects.property(String.class).convention("");
+        this.publicationGroup = objects.property(String.class);
+        this.publicationVersion = objects.property(String.class);
         this.publicationArtifactId = objects.property(String.class);
-        this.publicationVersion = objects.property(String.class).convention("");
         this.publicationType = objects.property(PublicationType.class).convention(PublicationType.LIBRARY);
     }
 
